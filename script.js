ff9f4d37dcd408e00c0c135db4041acf2c4a2600
fanuelnalem <fanuelnalem@outlook.com>
2020-01-14 20:56:04 -0800

    update

---
+++ script.js
@@ -0,0 +1,16 @@
+ 
+function pass(){
+  var result=''
+
+for( var i=0;i<8;i++){
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+=123456789";
+  var randomgenerator = Math.floor(Math.random()*73);
+  result += characters.charAt(randomgenerator);
+ 
+}
+ 
+document.getElementById("generate").value = result;
+}
+ 
+
+ 

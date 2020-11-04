8e75635aa83beeeea16035de56ea5e151db37e00
Fanuel Alem <fanuelnalem@outlook.com>
2020-11-03 19:01:33 -0800

    insert the password into the text

--- script.js
+++ script.js
@@ -217,3 +217,17 @@ function generatePassword() {
   // Transform the result into a string and pass into writePassword
   return result.join('');
 }
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
